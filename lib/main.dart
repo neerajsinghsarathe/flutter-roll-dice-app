52f0d427189918a953803a8272780f0d4064964a
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2023-10-25 04:32:55 +0530

    resuable gradient component created

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 6, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ),
       ),
     ),
   );
