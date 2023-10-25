c289002f05d1f726e5e4b5edc43ff58c8e253632
Neeraj Singh Sarathe <neerajssarathe@gmail.com>
2023-10-25 05:51:36 +0530

    created a stateful component to make changes in the widget with the help of setState function.
    In stateless widget all the variables should be final and thus cannot be used.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,18 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
+   const GradientContainer(this.color1, this.color2, {super.key});
 
-  const GradientContainer.purple({super.key}) :
+   const GradientContainer.purple({super.key}) :
       color1 = Colors.deepPurple,
       color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
-
-  void rollDice(){
-
-  }
 
   @override
   Widget build(context) {
@@ -28,24 +24,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-2.png', width: 200,),
-            const SizedBox(height: 20,),
-            TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  /*padding: const EdgeInsets.only(top: 20,),*/
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  )
-                ),
-                child: const Text('Roll dice'))
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
 
       ),
     );
